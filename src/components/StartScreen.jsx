47547fc871aaa494048461d0327ffe91518b2f88
mohammad sadra nafe <sadranafe7@gmail.com>
2023-11-03 15:29:07 +0330

    Btn.jsx added to StartScreen.jsx

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,21 @@
+import Btn from "./basic_components/Btn";
+
+const StartScreen = ({ QuestionsLength , dispatch }) => {
+    return (
+        <>
+            <div className = "w-full h-full flex flex-wrap justify-center items-center content-center">
+                <div className = "w-full text-center mb-5">
+                    <h1 className = "text-4xl font-bold">Welcome to the React Quiz !</h1>
+                </div>
+
+                <p className = "text-lg">{QuestionsLength} questions to test your react mastery</p>
+
+                <div className = "w-full text-center pt-5">
+                    <Btn content = "let's start" type = 'start' dispatch = {dispatch}/>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default StartScreen;
