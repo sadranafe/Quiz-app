c7b71f031f773431ba683ff56d63dc9f7ffa245c
mohammad sadra nafe <sadranafe7@gmail.com>
2023-11-03 20:25:06 +0330

    change the logic of the steps in ProgressQuiz.jsx

--- src/components/ProgressQuiz.jsx
+++ src/components/ProgressQuiz.jsx
@@ -1,11 +1,12 @@
 
 const ProgressQuiz = ({ index ,  QuestionsLength , maxPossiblePoints , answer , points }) => {
+    // Number(answer !== null) --> it returns 1 if it is true and if it is false it returns 0
     return (
         <>
             <div className = "w-full flex flex-wrap justify-evenly items-center my-3 mb-5">
                 
                 <div className = "w-full text-center flex flex-wrap justify-center items-center mb-3">
-                    <progress value = {answer !== null ?  index + 1 : index} max = {QuestionsLength} className = "w-[520px] h-2"></progress>
+                    <progress value = {index + Number(answer !== null)} max = {QuestionsLength} className = "w-[520px] h-2"></progress>
                 </div>
 
                 <div>
