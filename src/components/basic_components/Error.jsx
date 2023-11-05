6cc81bcc1da0e7107f989cc265f4170f1761362d
mohammad sadra nafe <sadranafe7@gmail.com>
2023-11-05 23:56:28 +0330

    the Component that handle the Errors created and imported in MainQuizContent.jsx

---
+++ src/components/basic_components/Error.jsx
@@ -0,0 +1,12 @@
+
+const Error = () => {
+    return (
+        <>
+            <h1 className = 'text-white text-xl'>
+                <span>💥</span> There was an <span className='text-red-500'>error</span> fetching questions !
+            </h1>
+        </>
+    );
+};
+
+export default Error;
