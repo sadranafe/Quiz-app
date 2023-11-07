47645c1764d7fe5f671ff9b5535c0979cf02a45c
mohammad sadra nafe <sadranafe7@gmail.com>
2023-11-07 19:01:29 +0330

    the logic of the validation of the options changed

--- src/components/question/Options.jsx
+++ src/components/question/Options.jsx
@@ -4,7 +4,7 @@ const Options = ({ option , answer , correctOption , dispatch , index }) => {
 
     return (
         <>
-            <button onClick = {() => dispatch({type : 'newAnswer' , payLoad : index})} disabled = {hasAnswer} className = {`flex justify-between items-center w-10/12 ${option.length >= 62 && 'text-sm'} ${hasAnswer ? (index === correctOption ? "bg-green-700" : "bg-orange-500") : 'hover:bg-transparent'} disabled:cursor-not-allowed p-3 px-3 box-border m-2 outline-none rounded-xl transition-all border border-neutral-700 bg-neutral-700 text-start`}>
+            <button onClick = {() => dispatch({type : 'newAnswer' , payLoad : index})} disabled = {hasAnswer} className = {`flex justify-between items-center w-10/12 ${option.length >= 62 && 'text-sm'} ${hasAnswer ? (index === correctOption ? "bg-green-600" : "bg-orange-500") : 'hover:bg-transparent bg-neutral-700'} disabled:cursor-not-allowed p-3 px-3 box-border m-2 outline-none rounded-xl transition-all border border-neutral-700 text-start`}>
                 <span>
                     {option}
                     <span className = {index === answer ? 'text-[11px] ml-3' : 'hidden'}>(your answer)</span>
